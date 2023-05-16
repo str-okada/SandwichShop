069aa48389566e0b987a2cfc63808432453f01e1
str-okada <shotarookadaa@gmail.com>
2023-05-16 14:35:53 -0700

    fiexed a little

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8084",
+  baseURL: "http://localhost:8080",
   headers: { "content-type": "application.json" },
 });
